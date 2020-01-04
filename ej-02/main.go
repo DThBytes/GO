d6ce3ab8c03990de7ef937409a25b028e86c39d4
DThBytes <dtheriano@gmail.com>
2020-01-03 22:43:33 -0300

    ej-02

---
+++ ej-02/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var num int
+var cadena string
+var opcion bool
+
+func main() {
+	numero1, numero2, texto := 5, 6, "Hola"
+	fmt.Println(numero1)
+	fmt.Println(numero2)
+	fmt.Println(texto)
+}
